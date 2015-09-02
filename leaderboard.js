30b2ce820ee4213345dad11fadd9c5eef6d9fd38
Kyle Henderson <kyle@Kyles-MBP.rhod.uc.edu>
2015-09-02 15:08:41 -0400

    set up next and prev to iterate through the list of players

--- leaderboard.js
+++ leaderboard.js
@@ -5,7 +5,17 @@ function generateID () {
   return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-var tiles = [{
+function renderTimeline (target) {
+  //console.log(target);
+}
+
+function filterPlayers (players, length, offset) {
+  offset = offset || 0;
+
+  return players.slice(offset, offset + length);
+}
+
+var players = [{
   userName: 'Hehk.8888',
   charName: '1',
   rank: 420,
@@ -31,62 +41,84 @@ var tiles = [{
   id: generateID()
 },{
   userName: 'Hehk.8888',
-  charName: 'Riggity Rekt',
+  charName: '5',
   rank: 420,
   winRate: 69.69,
   id: generateID()
 },{
   userName: 'Hehk.8888',
-  charName: 'Riggity Rekt',
+  charName: '6',
   rank: 420,
   winRate: 69.69,
   id: generateID()
 },{
   userName: 'Hehk.8888',
-  charName: 'Riggity Rekt',
+  charName: '7',
   rank: 420,
   winRate: 69.69,
   id: generateID()
 },{
   userName: 'Hehk.8888',
-  charName: 'Riggity Rekt',
+  charName: '8',
   rank: 420,
   winRate: 69.69,
   id: generateID()
 },{
   userName: 'Hehk.8888',
-  charName: 'Riggity Rekt',
+  charName: '9',
   rank: 420,
   winRate: 69.69,
   id: generateID()
 },{
   userName: 'Hehk.8888',
-  charName: 'Riggity Rekt',
+  charName: '10',
   rank: 420,
   winRate: 69.69,
   id: generateID()
 },{
   userName: 'Hehk.8888',
-  charName: 'Riggity Rekt',
+  charName: '11',
   rank: 420,
   winRate: 69.69,
   id: generateID()
 },{
   userName: 'Hehk.8888',
-  charName: 'Riggity Rekt',
+  charName: '12',
   rank: 420,
   winRate: 69.69,
   id: generateID()
 },{
   userName: 'Hehk.8888',
-  charName: 'Riggity Rekt',
+  charName: '13',
   rank: 420,
   winRate: 69.69,
   id: generateID()
 }];
 
 if (Meteor.isClient) {
+  var generalTilesSettings = {
+    length: 5,
+    offset: 0,
+    prevActive: function () {
+      var target = $('#general').find('.prev');
+      if (this.offset > 0) {
+        target.removeClass('inactive');
+      } else {
+        target.addClass('inactive');
+      }
+    },
+    nextActive: function () {
+      var target = $('#general').find('.next');
+      if (this.offset + this.length < players.length) {
+        target.removeClass('inactive');
+      } else {
+        target.addClass('inactive');
+      }
+    }
+  };
   var tileClick = function (event) {
+    $('.tile-info').remove();
+
     if (event.target.className === 'tile') {
       $('.tile.selected').removeClass('selected').removeClass('first');
       if (event.target.parentElement.firstElementChild === event.target) {
@@ -94,14 +126,15 @@ if (Meteor.isClient) {
       } else {
         event.target.className = 'tile selected';
       }
+
+      Blaze.renderWithData(Template.tileInfo, {}, event.target);
+
+      renderTimeline(d3.select(event.target.querySelector('.tile-info'))
+          .append('div')
+          .attr('class', 'timeline'));
     } else {
       event.target.className = 'tile';
     }
-
-    $('.tile-info').remove();
-    Blaze.renderWithData(Template.tileInfo, {}, event.target);
-
-
   }, adjustScroll = function (delta) {
     var target = $(window);
     target.scrollTop(target.scrollTop() + delta);
@@ -114,15 +147,18 @@ if (Meteor.isClient) {
   });
 
   Template.favorites.helpers({
+    isPopulated: function () {
+      return Session.get('favorites').length > 0;
+    },
     players: function () {
       var favorites = Session.get('favorites'),
           content = [];
 
-      for (var player in tiles) {
-        if (tiles.hasOwnProperty(player) &&
-            favorites.indexOf(tiles[player].id) !== -1)
+      for (var player in players) {
+        if (players.hasOwnProperty(player) &&
+            favorites.indexOf(players[player].id) !== -1)
         {
-            content.push(tiles[player]);
+            content.push(players[player]);
         }
       }
 
@@ -134,11 +170,36 @@ if (Meteor.isClient) {
     'click .tile' : tileClick
   });
 
+  Session.set('generalPlayers', filterPlayers(players, generalTilesSettings.length));
   Template.general.helpers({
-    players: tiles
+    players: function () {
+      return Session.get('generalPlayers');
+    }
   });
 
   Template.general.events({
+    'click .next' : function (event) {
+      if (generalTilesSettings.offset < players.length) {
+        generalTilesSettings.offset = generalTilesSettings.offset + generalTilesSettings.length;
+        Session.set('generalPlayers', filterPlayers(players, generalTilesSettings.length, generalTilesSettings.offset));
+      }
+
+      generalTilesSettings.prevActive();
+      generalTilesSettings.nextActive();
+      $('.tile-info').remove();
+      $('#general').find('.tile').removeClass('selected');
+    },
+    'click .prev' : function (event) {
+      if (generalTilesSettings.offset > 0) {
+        generalTilesSettings.offset = generalTilesSettings.offset - generalTilesSettings.length;
+        Session.set('generalPlayers', filterPlayers(players, generalTilesSettings.length, generalTilesSettings.offset));
+      }
+
+      generalTilesSettings.prevActive();
+      generalTilesSettings.nextActive();
+      $('.tile-info').remove();
+      $('#general').find('.tile').removeClass('selected');
+    },
     'click .tile' : function (event) {
       var favorites = Session.get('favorites'),
           id = event.target.getAttribute('id');
@@ -155,6 +216,11 @@ if (Meteor.isClient) {
       tileClick(event);
     }
   });
+
+  Template.general.rendered = function () {
+    generalTilesSettings.prevActive();
+    generalTilesSettings.nextActive();
+  };
 
 }
 
